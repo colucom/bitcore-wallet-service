c0fe04028e8ed803f050a6e1c7b58104d8db28b7
valery-zhurbenko <valery-zhurbenko@users.noreply.github.com>
2016-08-28 17:54:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,23 +10,23 @@ run_program ()
   pidfile=$2
   logfile=$3
 
-  if [ -e "$pidfile" ]
-  then
-    echo "$nodefile is already running. Run 'npm stop' if you wish to restart."
-    return 1
-  fi
+  # if [ -e "$pidfile" ]
+  # then
+  #   echo "$nodefile is already running. Run 'npm stop' if you wish to restart."
+  #   return 0
+  # fi
 
   nohup node $nodefile >> $logfile 2>&1 &
-  PID=$!
-  if [ $? -eq 0 ]
-  then
-    echo "Successfully started $nodefile. PID=$PID. Logs are at $logfile"
-    echo $PID > $pidfile
-    return 0
-  else
-    echo "Could not start $nodefile - check logs at $logfile"
-    exit 1
-  fi
+  # PID=$!
+  # if [ $? -eq 0 ]
+  # then
+  #   echo "Successfully started $nodefile. PID=$PID. Logs are at $logfile"
+  #   echo $PID > $pidfile
+  #   return 0
+  # else
+  #   echo "Could not start $nodefile - check logs at $logfile"
+  #   exit 1
+  # fi
 }
 
 run_program locker/locker.js pids/locker.pid logs/locker.log
